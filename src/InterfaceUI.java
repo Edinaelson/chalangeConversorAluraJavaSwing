3054898ad390758b4f03a8d721e981dd9c3216d1
EdinaelsonGaleno <sedinaelson@gmail.com>
2023-03-31 20:10:31 -0300

    refatorando interface

--- src/InterfaceUI.java
+++ src/InterfaceUI.java
@@ -5,16 +5,14 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 public class InterfaceUI extends JFrame {
-
     final DecimalFormat df = new DecimalFormat("####,#00.00");
     JTextField jTextField = new JTextField();
-    JLabel resultado = new JLabel("Resultado");
-
+    JLabel result = new JLabel("Resultado");
     public InterfaceUI() {
-
         super("Conversor");
-        JLabel fieldDolar, fieldEuro, fieldLibras, fieldPesoArgentino, fieldPesoChileno, fieldReal;
+
         JLabel fieldUSD,fieldEUR,fieldGBP,fieldARS,fieldCLP,fieldBRL;
+        JLabel fieldDolar, fieldEuro, fieldLibras, fieldPesoArgentino, fieldPesoChileno, fieldReal;
 
         fieldDolar = new JLabel("Dólar");
         fieldEuro = new JLabel("Euro");
@@ -50,35 +48,33 @@ public class InterfaceUI extends JFrame {
 
         JFrame jFrame = new JFrame();
 
+        JComboBox<String> jComboBox = new JComboBox<>(optionCoin);
         JComboBox<String> jComboBox1 = new JComboBox<>(optionsToChoose);
+        jComboBox.setBounds(15,50,60,20);
         jComboBox1.setBounds(180, 50, 100, 20);
 
-        JComboBox<String> jComboBox2 = new JComboBox<>(optionCoin);
-        jComboBox2.setBounds(15,50,60,20);
-
         JButton jButton = new JButton("Selecionar");
+
         JLabel jLabel = new JLabel();
 
         jTextField.setBounds(80, 50, 90, 20);
         jButton.setBounds(180, 80, 100, 20);
-        resultado.setBounds(140, 200, 100, 60);
+        result.setBounds(140, 200, 100, 60);
         jLabel.setBounds(90, 100, 400, 100);
 
         // estilização
-        int buttonWidth = 100;
+        int btnWidth = 100;
         int buttonHeight = 30;
-
         jComboBox1.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
-        jComboBox1.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
+        jComboBox1.setPreferredSize(new Dimension(btnWidth, buttonHeight));
+        jComboBox.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        jComboBox.setPreferredSize(new Dimension(btnWidth, buttonHeight));
 
-        jComboBox2.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
-        jComboBox2.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
-
-        resultado.setFont(new Font("Verdana", Font.BOLD, 15));
-        resultado.setForeground(new Color(0, 0, 0));
+        result.setFont(new Font("Verdana", Font.BOLD, 15));
+        result.setForeground(new Color(0, 0, 0));
 
         jButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-        jButton.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
+        jButton.setPreferredSize(new Dimension(btnWidth, buttonHeight));
         jButton.setForeground(Color.WHITE);
         jButton.setBackground(new Color(0, 0, 0));
         //fim estilizaçã0
@@ -87,18 +83,16 @@ public class InterfaceUI extends JFrame {
         jFrame.add(jComboBox1);
         jFrame.add(jLabel);
         jFrame.add(jTextField);
-        jFrame.add(resultado);
-        jFrame.add(jComboBox2);
+        jFrame.add(result);
+        jFrame.add(jComboBox);
 
         jFrame.setLayout(null);
         jFrame.setSize(350, 400);
         jFrame.setVisible(true);
 
         jButton.addActionListener(e -> {
-
             int choice1 = jComboBox1.getSelectedIndex();
-            int choice2 = jComboBox2.getSelectedIndex();
-
+            int choice2 = jComboBox.getSelectedIndex();
             try {
                 bConverterClick(choice1,choice2);
             } catch (NumberFormatException | IOException exc) {
@@ -108,14 +102,12 @@ public class InterfaceUI extends JFrame {
             }
         });
     }
-
     private void bConverterClick(int vrl1, int vrl2) throws IOException {
         if (!jTextField.getText().equals("")) {
             try {
                 BigDecimal tfValor1Big = new BigDecimal(jTextField.getText());
                 BigDecimal res = Converter.convertCurrency(tfValor1Big,vrl1,vrl2);
-                jTextField.setText("");
-                resultado.setText(df.format(res));
+                result.setText(df.format(res));
 
             } catch (NumberFormatException exc) {
                 jTextField.selectAll();
